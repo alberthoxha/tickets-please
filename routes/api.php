6bb195a1845b28580944bf87bc97627e10ac5574
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2025-11-19 23:55:12 +0100

    Refactor AuthController to use ApiLoginRequest for login validation and update routes for login and register endpoints

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, ]);
 
 Route::get('/user', function (Request $request) {
     return $request->user();

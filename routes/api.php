5e203788c97f668d440f6fcc75bbefff275070c8
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2025-11-22 15:15:16 +0100

    feat: add API versioning support by including api_v1 routes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::prefix('v1')->group(function () {
+    require base_path('routes/api_v1.php');
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
